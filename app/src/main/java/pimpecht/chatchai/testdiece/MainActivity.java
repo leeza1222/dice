688145f9e2817bad2553070802851a877738fbb0
leeza1222@gmail.com <bank030347941>
2016-02-24 11:56:57 +0700

    ทำปุ่ม back next random เรียบร้อยเเล้ว!!

--- app/src/main/java/pimpecht/chatchai/testdiece/MainActivity.java
+++ app/src/main/java/pimpecht/chatchai/testdiece/MainActivity.java
@@ -2,32 +2,65 @@ package pimpecht.chatchai.testdiece;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private ImageView image1;
     private  Button BackButton, RandomButton, DiceButton;
+    private int intdic = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image1 = (ImageView) findViewById(R.id.imageView);
 
         bindWidget();
         BackController();
         RandomController();
         DiceController();
+        changeplay();
 
 
     } //bindWidget
+
+    private void changeplay() {
+
+
+        switch (intdic) {
+            case 1:
+                image1.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                image1.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                image1.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                image1.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                image1.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                image1.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+
 
     private void DiceController() {
         DiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
 
             }
         });
@@ -38,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
+
             }
         });
     }
@@ -47,12 +82,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                myRandomPicture(1);
+                intdic -= 1;
+                if (intdic == 0) {
+                    intdic = 6;
+                }
+                changeplay();
+
+
+
             }
         });
     }
 
+    private void myRandomPicture(int i) {
+
+    }
+
     private void bindWidget() {
-        imageView = (ImageView) findViewById(R.id.imageView);
+        image1 = (ImageView) findViewById(R.id.imageView);
         BackButton = (Button) findViewById(R.id.BackButton);
         RandomButton = (Button) findViewById(R.id.RandomButton);
         DiceButton = (Button) findViewById(R.id.DiceButton);
