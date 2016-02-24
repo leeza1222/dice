8f689c6090f83cc37b1377ecbf49c8d7e3c3bf92
leeza1222@gmail.com <bank030347941>
2016-02-24 10:54:11 +0700

    bindWidget เรียบร้อยเเล้ว!!

--- app/src/main/java/pimpecht/chatchai/testdiece/MainActivity.java
+++ app/src/main/java/pimpecht/chatchai/testdiece/MainActivity.java
@@ -2,12 +2,31 @@ package pimpecht.chatchai.testdiece;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private  Button BackButton, RandomButton, DiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+
+
+    } //bindWidget
+
+    private void bindWidget() {
+        imageView = (ImageView) findViewById(R.id.imageView);
+        BackButton = (Button) findViewById(R.id.BackButton);
+        RandomButton = (Button) findViewById(R.id.RandomButton);
+        DiceButton = (Button) findViewById(R.id.DiceButton);
+
     }
+
+
 }
